2b97d147cbef5c97c7771b04c4ab32b023b300fe
agajews <agajews@gmail.com>
2020-04-17 23:41:35 -0400

    properly marking up html? but it puts some marks on the inside of tags...

--- new_html_parser.py
+++ new_html_parser.py
@@ -1,39 +1,77 @@
 from html.parser import HTMLParser
+from difflib import SequenceMatcher
 
 
 self_closing = ["br"]
+whitespace = [" ", "\t", "\n"]
 
 
 def split_words(data):
     words = []
-    word = ""
-    for char in data:
-        word += char
-        if char in [" ", "\t", "\n"]:
-            words.append(word)
-            word = ""
-    if word:
-        words.append(word)
+    start = 0
+    for i in range(len(data)):
+        if i == len(data) - 1:
+            words.append(data[start:])
+            break
+        if data[i] in whitespace and data[i + 1] not in whitespace:
+            words.append(data[start : i + 1])
+            start = i + 1
     return words
 
 
-class DataToken:
+def immutify(data):
+    if isinstance(data, tuple) or isinstance(data, list):
+        return tuple(immutify(x) for x in data)
+    else:
+        return data
+
+
+class Token:
+    def __init__(self, identity):
+        self.identity = immutify(identity)
+
+    def __eq__(self, other):
+        return self.identity == other.identity
+
+    def __hash__(self):
+        return hash(self.identity)
+
+
+class DataToken(Token):
     def __init__(self, data, context):
+        super().__init__((data, context))
         self.data = data
         self.context = context
+        self.inserted = False
+        self.deleted = False
 
     def __repr__(self):
-        return "{}({})".format(repr(self.data), ",".join(t for t, a in self.context))
+        ins = ""
+        if self.inserted:
+            ins = "{ins}"
+        elif self.deleted:
+            ins = "{del}"
+        return "{}({}){}".format(
+            repr(self.data), ",".join(t for t, a in self.context), ins
+        )
 
 
-class TagToken:
+class TagToken(Token):
     def __init__(self, tag, context, attrs):
+        super().__init__((tag, context, attrs))
         self.tag = tag
         self.context = context
         self.attrs = attrs
+        self.inserted = False
+        self.deleted = False
 
     def __repr__(self):
-        return "<{}>({})".format(self.tag, ",".join(t for t, a in self.context))
+        ins = ""
+        if self.inserted:
+            ins = "{ins}"
+        elif self.deleted:
+            ins = "{del}"
+        return "<{}>({}){}".format(self.tag, ",".join(t for t, a in self.context), ins)
 
 
 class HTMLSequencer(HTMLParser):
@@ -111,16 +149,114 @@ def generate_html(sequence):
     return "".join(html)
 
 
-print(split_words("hello world!"))
-print(split_words("hello world! "))
-parser = HTMLSequencer()
-parser.feed(
-    "<h1>This is <em>a</em> header</h1>\n\n"
-    "<div><p>This is the <strong>beginning</strong> of <br><br>my paragraph.</p></div>\n\n"
-    "<h2><strong><em>This is a <a href='https://google.com'>sub-header</a></em></strong></h2>\n"
-    "<div><p>This is the <em>middle</em> of my document.</p></div>"
-    "<div>This is a bulleted list: <ul><li>yo</li><li>yolo</li></ul></div>"
-)
-print("\n".join(repr(t) for t in parser.sequence))
-print("\n\n===HTML===")
-print(generate_html(parser.sequence))
+def startswith(l, prefix):
+    if prefix is None:
+        return False
+    return l[: len(prefix)] == prefix
+
+
+def insert_diff_tags(sequence):
+    insert_prefix = None
+    delete_prefix = None
+    for token in sequence:
+        if token.inserted:
+            if startswith(token.context, insert_prefix):
+                token.context.insert(len(insert_prefix), [("ins", [])])
+            else:
+                insert_prefix = token.context
+                token.context.append(("ins", []))
+        else:
+            insert_prefix = None
+
+        if token.deleted:
+            if startswith(token.context, delete_prefix):
+                token.context.insert(len(delete_prefix), [("del", [])])
+            else:
+                delete_prefix = token.context
+                token.context.append(("del", []))
+        else:
+            delete_prefix = None
+
+
+def get_sequence(data):
+    parser = HTMLSequencer()
+    parser.feed(data)
+    return parser.sequence
+
+
+def markup_changes(data_a, data_b):
+    sequence_a = get_sequence(data_a)
+    sequence_b = get_sequence(data_b)
+    matcher = SequenceMatcher(isjunk=None, a=sequence_a, b=sequence_b, autojunk=False)
+    merged_sequence = []
+    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        if tag == "equal":
+            merged_sequence += sequence_b[j1:j2]
+        if tag == "replace" or tag == "delete":
+            for token in sequence_a[i1:i2]:
+                token.deleted = True
+                merged_sequence.append(token)
+        if tag == "replace" or tag == "insert":
+            for token in sequence_b[j1:j2]:
+                token.inserted = True
+                merged_sequence.append(token)
+    # print("\n".join(repr(t) for t in merged_sequence))
+    # print("=========")
+    insert_diff_tags(merged_sequence)
+    return generate_html(merged_sequence)
+
+
+data_a = """
+<h1>This is just a header</h1>
+<div>This is a body paragraph that goes along with it.</div>
+
+<h2>This is another header</h2>
+<div>This body paragraph has a bulleted list:
+    <ul>
+        <li>yo</li>
+        <li>yoyo</li>
+        <li>This one is nested:<ul>
+            <li>yo</li>
+            <li>yoyo</li>
+        </ul></li>
+    </ul>
+    This is the end of the paragraph.
+</div>
+"""
+
+data_b = """
+<h1>This is a header</h1>
+<div>This is a related paragraph that goes along with it.</div>
+
+<h2>This is <strong>another</strong> header</h2>
+<div>This is an entirely new <em>body</em> paragraph.</div>
+<div>This body paragraph has a bulleted list:
+    <ul>
+        <li>yo</li>
+        <li>hyohyo</li>
+        <li>This one is nested:<ul><ul>
+            <li>yo</li>
+            <li>yoyo</li>
+        </ul></ul></li>
+    </ul>
+    This is the end of the paragraph.
+</div>
+"""
+
+
+print(markup_changes(data_a, data_b))
+
+
+# print(split_words("hello world!"))
+# print(split_words("hello world! "))
+# parser = HTMLSequencer()
+# parser.feed(
+#     "<h1>This is <em>a</em> header</h1>\n\n"
+#     "<div><p>This is the <strong>beginning</strong> of <br><br>my paragraph.</p></div>\n\n"
+#     "<h2><strong><em>This is a <a href='https://google.com'>sub-header</a></em></strong></h2>\n"
+#     "<div><p>This is the <em>middle</em> of my document.</p></div>"
+#     "<div>This is a bulleted list: <ul><li>yo</li><li>yolo</li></ul></div>"
+# )
+# print("\n".join(repr(t) for t in parser.sequence))
+# print("\n\n===HTML===")
+# print(generate_html(parser.sequence))
