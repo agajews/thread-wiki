54bade50dc9674a67fd153427ad4fb7cd136e710
agajews <agajews@gmail.com>
2020-04-20 23:46:39 -0400

    added implicit url changes

--- server/server.py
+++ server/server.py
@@ -25,7 +25,9 @@ def inject_url_for_title():
 
 @app.route("/")
 def index():
-    pages = db.pages.find({}, {"titles": {"$slice": -1}, "headings": {"$slice": -1}})
+    pages = db.pages.find(
+        {}, {"titles": {"$slice": -1}, "versions.heading": {"$slice": -1}}
+    )
     return render_template("index.html", pages=pages)
 
 
@@ -45,10 +47,7 @@ def index():
 
 
 def find_page(title):
-    page = db.pages.find_one(
-        {"titles": title},
-        {"versions": {"$slice": -1}, "headings": {"$slice": -1}, "type": 1},
-    )
+    page = db.pages.find_one({"titles": title}, {"versions": {"$slice": -1}, "type": 1})
     return page
 
 
@@ -68,7 +67,7 @@ def is_valid_email(email):
 
 
 def generate_user_template(email):
-    return "<p>The homo sapiens {} is simply the best.</p><h2>Best Quotes</h2><p>Yoyoyo, it's my boy {}</p><h2>Early life</h2>One day, our protagonist {} was born. Later, they went to college.</p>".format(
+    return "<p>The homo sapiens {0} is simply the best.</p><h2>Best Quotes</h2><p>Yoyoyo, it's my boy {0}</p><h2>Early life</h2>One day, our protagonist {0} was born. Later, they went to college.</p>".format(
         email
     )
 
@@ -78,27 +77,28 @@ def page(title):
     page = find_page(title)
     if page is not None and page["type"] == "user":
         return render_template(
-            "user-page.html",
-            body=page["versions"][-1]["body"],
-            heading=page["headings"][-1],
-            title=title,
+            "user-page.html", version=page["versions"][-1], title=title
         )
     if g.user is None:
         abort(404)
     if is_valid_email(title):
-        db.pages.insert(
+        db.pages.insert_one(
             {
                 "titles": [title],
-                "headings": [title],
                 "type": "user",
                 "versions": [
                     {
                         "body": generate_user_template(title),
+                        "heading": title,
                         "editor": g.user["_id"],
                         "timestamp": timestamp(),
                     }
                 ],
             }
+        )
+        page = find_page(title)
+        return render_template(
+            "user-page.html", version=page["versions"][-1], title=title
         )
     abort(404)  # eventually, create new topic page
 
@@ -110,10 +110,7 @@ def edit(title):
         abort(401)
     if page is not None and page["type"] == "user":
         return render_template(
-            "edit-user-page.html",
-            body=page["versions"][-1]["body"],
-            heading=page["headings"][-1],
-            title=title,
+            "edit-user-page.html", version=page["versions"][-1], title=title
         )
     abort(404)
 
@@ -134,25 +131,31 @@ def submitedit(title):
     if data is None:
         abort(400)
     body = data.get("body")
-    if body is None:
+    heading = data.get("heading")
+    if body is None or heading is None:
         abort(400)
     if g.user is None:
         abort(401)
+    page = db.pages.find_one({"titles": title}, {"titles": 1})
+    if page is None:
+        abort(400)
     sanitized_body = sanitize_html(body)
-    update = db.pages.update_one(
-        {"titles": title},
-        {
-            "$push": {
-                "versions": {
-                    "body": sanitized_body,
-                    "editor": g.user["_id"],
-                    "timestamp": timestamp(),
-                }
-            }
-        },
-    )
+    push = {
+        "versions": {
+            "body": sanitized_body,
+            "heading": heading,
+            "editor": g.user["_id"],
+            "timestamp": timestamp(),
+        }
+    }
+    newtitle = heading.replace(" ", "_")
+    if newtitle not in page["titles"]:
+        push["titles"] = newtitle
+    update = db.pages.update_one({"titles": title}, {"$push": push})
     if update.modified_count > 0:
-        return jsonify({"success": True})
+        return jsonify(
+            {"success": True, "redirect": url_for_title("page", title=newtitle)}
+        )
     return jsonify(
         {"success": False, "html": {"editerror": "Oops, something went wrong :("}}
     )
